ab140648e99ae9481c72932435ea7bbf98431e50
Agastya Vashisht <23bai70472@cuchd.in>
2025-09-30 16:41:39 +0530

    commiting Practical

---
+++ React-Project/react-mst/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayerList from "./component/PlayerList";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Sports Player List</h1>
+      <PlayerList />
+    </div>
+  );
+}
+
+export default App;
